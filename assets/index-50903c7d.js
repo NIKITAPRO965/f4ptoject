0a5e948312adbf53b23c032fb8e705baf9112487
NIKITAPRO965 <nikita.pro.965@gmail.com>
2026-06-06 12:03:56 +0000

    Deploying to gh-pages from @ NIKITAPRO965/f4ptoject@90d343ffe2a8f7160ac7ac5e9ff0936cdf611e67 🚀

--- assets/index-50903c7d.js
+++ assets/index-50903c7d.js
@@ -1,33 +1,33 @@
-(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))d(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const l of t.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&d(l)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function d(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const u="/f4ptoject/assets/logo-90450bbb.png";document.querySelector(".header__img").src=u;const v=document.querySelector("");v.addEventListener("click",i=>{const s=i.target.closest(".events__item");if(!s)return;const{name:o,date:d,venue:e,city:t,image:l,standard:_,info:p}=s.dataset;let n=s.dataset.vip;(!n||n==="N/A")&&(n=Number(_)*2);const a=document.createElement("div");a.classList.add("backdrop"),a.innerHTML=`
+(function(){const a=document.createElement("link").relList;if(a&&a.supports&&a.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))n(t);new MutationObserver(t=>{for(const e of t)if(e.type==="childList")for(const l of e.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&n(l)}).observe(document,{childList:!0,subtree:!0});function d(t){const e={};return t.integrity&&(e.integrity=t.integrity),t.referrerPolicy&&(e.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?e.credentials="include":t.crossOrigin==="anonymous"?e.credentials="omit":e.credentials="same-origin",e}function n(t){if(t.ep)return;t.ep=!0;const e=d(t);fetch(t.href,e)}})();const p="/f4ptoject/assets/logo-90450bbb.png";document.querySelector(".header__img").src=p;const m=document.querySelector(".btn-open-modal");m.addEventListener("click",()=>{const o="TEST",a="20:00",d="Main Hall",n="Kyiv",t="https://via.placeholder.com/150",l="TEST INFO";let i=200;const s=document.createElement("div");s.classList.add("backdrop"),s.innerHTML=`
     <div class="modal">
       <button class="modal__close-btn" type="button">×</button>
 
       <div class="modal__avatar-wrap">
-        <img class="modal__avatar-img" src="${l}" alt="${o}" />
+        <img class="modal__avatar-img" src="${t}" alt="${o}" />
       </div>
 
       <div class="modal__container">
         <div class="modal__poster-wrap">
-          <img class="modal__poster" src="${l}" alt="${o}" />
+          <img class="modal__poster" src="${t}" alt="${o}" />
         </div>
 
         <div class="modal__content">
           <div class="modal__group modal__group--info">
             <span class="modal__label">INFO</span>
-            <p class="modal__text modal__text--description">${p}</p>
+            <p class="modal__text modal__text--description">${l}</p>
           </div>
 
           <h2 class="modal__title">${o}</h2>
 
           <div class="modal__group">
             <span class="modal__label">WHEN</span>
-            <p class="modal__text">${d}</p>
+            <p class="modal__text">${a}</p>
             <p class="modal__subtext">20:00 (Kyiv/Ukraine)</p>
           </div>
 
           <div class="modal__group">
             <span class="modal__label">WHERE</span>
-            <p class="modal__text">${t}, ${e}</p>
+            <p class="modal__text">${n}, ${d}</p>
           </div>
 
           <div class="modal__group">
@@ -41,7 +41,7 @@
             <div class="modal__price-row">
               <p class="modal__text">
                 <span class="modal__barcode">║▌║█║▌│║</span>
-                Standart ${_}
+                Standart 100
               </p>
               <button class="modal__btn" type="button">
                 BUY TICKETS
@@ -51,7 +51,7 @@
             <div class="modal__price-row">
               <p class="modal__text">
                 <span class="modal__barcode">║▌║█║▌│║</span>
-                VIP ${n}
+                VIP ${i}
               </p>
               <button class="modal__btn" type="button">
                 BUY TICKETS
@@ -67,4 +67,4 @@
         </button>
       </div>
     </div>
-  `,document.body.append(a);const c=()=>a.remove();a.querySelector(".modal__close-btn").addEventListener("click",c),a.addEventListener("click",r=>{r.target===a&&c()}),document.addEventListener("keydown",function r(m){m.key==="Escape"&&(c(),document.removeEventListener("keydown",r))})});
+  `,document.body.append(s);const c=()=>s.remove();s.querySelector(".modal__close-btn").addEventListener("click",c),s.addEventListener("click",r=>{r.target===s&&c()}),document.addEventListener("keydown",function r(_){_.key==="Escape"&&(c(),document.removeEventListener("keydown",r))})});
