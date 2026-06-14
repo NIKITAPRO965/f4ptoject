e8808f758a5d429edd737affa813843782af24f0
NIKITAPRO965 <nikita.pro.965@gmail.com>
2026-06-14 13:10:29 +0300

    header img fix

--- src/js/header.js
+++ src/js/header.js
@@ -1,2 +1,2 @@
-import logo from '../img/logo.png';
-document.querySelector('.header__img').src = logo;
+// import logo from '../img/logo.png';
+// document.querySelector('.header__img').src = logo;
